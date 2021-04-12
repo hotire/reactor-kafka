d04d5cba0fdb5705e62dd3c95f1c441148e862d6
hotire <gngh0101@gmail.com>
2021-04-13 00:51:54 +0900

    Add send to SampleProducer

--- src/main/kotlin/com/github/hotire/reactor/kafka/SampleProducer.kt
+++ src/main/kotlin/com/github/hotire/reactor/kafka/SampleProducer.kt
@@ -6,6 +6,10 @@ import reactor.kafka.sender.SenderOptions
 class SampleProducer(props: Map<String, Any>) : AutoCloseable {
     private val sender: KafkaSender<Int, String> = KafkaSender.create(SenderOptions.create(props))
 
+    fun send(topic: String, message: String) {
+        // TODO
+    }
+
     override fun close() {
         sender.close()
     }
