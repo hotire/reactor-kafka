a19c2f4faf5aa1bf0fbf516df7e2299a5db4a62e
hotire <gngh0101@gmail.com>
2021-04-13 00:50:41 +0900

    Implement AutoCloseable

--- src/main/kotlin/com/github/hotire/reactor/kafka/SampleProducer.kt
+++ src/main/kotlin/com/github/hotire/reactor/kafka/SampleProducer.kt
@@ -3,6 +3,10 @@ package com.github.hotire.reactor.kafka
 import reactor.kafka.sender.KafkaSender
 import reactor.kafka.sender.SenderOptions
 
-class SampleProducer(props: Map<String, Any>) {
+class SampleProducer(props: Map<String, Any>) : AutoCloseable {
     private val sender: KafkaSender<Int, String> = KafkaSender.create(SenderOptions.create(props))
+
+    override fun close() {
+        sender.close()
+    }
 }
